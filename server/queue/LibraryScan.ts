e766dd2d971fa2afce3b4463d617a70bd5d0b79f
Meow VPS <undefined@null.com>
2022-08-27 12:45:31 -0400

    use async for scans

--- server/queue/LibraryScan.ts
+++ server/queue/LibraryScan.ts
@@ -1,40 +1,53 @@
-import fs from "fs";
+import { promises as fs } from "fs";
 import { join, basename } from "path";
 import { Media } from "../server";
 import { ArtistMeta } from "../struct";
 
 Media.addEvent("LibraryScan", async (a) => {
   console.log("Scanning artist folders...");
-  const artists = fs.readdirSync(Media.dir);
+  const artists = await fs.readdir(Media.dir);
   const newData = [];
-  artists.forEach((a) => {
-    try {
-      const path = join(Media.dir, a);
-      const files = fs.readdirSync(path);
-      if (!files.includes("artist.json")) return;
-      const meta = JSON.parse(
-        fs.readFileSync(join(path, "artist.json")).toString()
-      ) as ArtistMeta;
-      if (meta.version !== 1)
-        return console.log(
-          `Old Artist Format in '${a}' (${meta.version || 0}). Please update.`
-        );
-      newData.push(meta);
+  await Promise.all(
+    artists.map(async (a) => {
+      try {
+        const path = join(Media.dir, a);
+        const files = await fs.readdir(path);
+        if (!files.includes("artist.json")) return;
+        const meta = JSON.parse(
+          (await fs.readFile(join(path, "artist.json"))).toString()
+        ) as ArtistMeta;
+        if (meta.version !== 1)
+          return console.log(
+            `Old Artist Format in '${a}' (${meta.version || 0}). Please update.`
+          );
+        newData.push(meta);
 
-      meta.albums.forEach((alb) => {
-        const path = Media.albumdir(meta, alb);
-        if (fs.existsSync(path)) {
-          const tracks = fs.readdirSync(path);
-          alb.tracks.forEach((t) => {
-            if (tracks.includes(basename(Media.trackdir(meta, alb, t))))
-              t.added = true;
-          });
-        }
-      });
-    } catch {
-      console.log(`Unrecognized Artist Folder: ${a}`);
-    }
-  });
+        await Promise.all(
+          meta.albums.map(async (alb) => {
+            const path = Media.albumdir(meta, alb);
+            if (
+              await (async () => {
+                try {
+                  return await fs.stat(path);
+                } catch {
+                  return false;
+                }
+              })()
+            ) {
+              const tracks = await fs.readdir(path);
+              alb.tracks.forEach((t) => {
+                if (tracks.includes(basename(Media.trackdir(meta, alb, t))))
+                  t.added = true;
+              });
+            }
+          })
+        );
+      } catch (err) {
+        console.log(`Unrecognized Artist Folder: ${a}`);
+        console.error(err);
+      }
+    })
+  );
   Media.artists = newData;
   console.log("Scan complete!");
 });
