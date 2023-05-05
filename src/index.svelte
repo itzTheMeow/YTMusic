38fbd81236467f437d78372ad501a45caacf87db
Meow <me@itsmeow.cat>
2023-05-05 14:48:27 -0400

    fix favicon

--- src/index.svelte
+++ src/index.svelte
@@ -14,6 +14,7 @@
   import { decodeTime } from "ulid";
   import { QueueActions } from "utils";
   import Home from "./Home.svelte";
+  import Logo from "./logo.png";
 
   if (!Auth.isAuthorized) window.location.href = "/login";
 
@@ -32,6 +33,10 @@
     clearInterval(setint);
   });
 </script>
+
+<svelte:head>
+  <link rel="icon" href={Logo} />
+</svelte:head>
 
 <div class="navbar bg-base-100" bind:this={nav}>
   <div class="navbar-start">
