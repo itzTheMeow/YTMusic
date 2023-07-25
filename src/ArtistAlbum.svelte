12cd0f2f8145fe273e12c7c8a889a93ba798e1d8
Meow <me@itsmeow.cat>
2023-07-25 16:59:12 +0000

    allow download

--- src/ArtistAlbum.svelte
+++ src/ArtistAlbum.svelte
@@ -1,33 +1,41 @@
 <script lang="ts">
-  import { IconDownload, IconExternalLink, IconTrash } from "@tabler/icons-svelte";
+  import {
+    IconDownload,
+    IconExternalLink,
+    IconTrash,
+  } from "@tabler/icons-svelte";
   import ArtistProviders from "ArtistProviders.svelte";
   import ArtistTrackAdd from "ArtistTrackAdd.svelte";
   import { API } from "index";
-  import Loader from "ui/Loader.svelte";
   import { Duration } from "luxon";
   import { onDestroy } from "svelte";
   import { link } from "svelte-routing";
   import { QALibraryScan } from "typings_queue";
   import type { Album, Artist } from "typings_struct";
+  import Loader from "ui/Loader.svelte";
 
   export let id: string;
   export let albid: string;
 
-  let albumDetails = new Promise<{ a: Artist; l: Album } | string>(async (r) => {
-    const res = await API.fetchArtist(id);
-    if (res.err) return r(res.message!);
-    const l = res.albums!.find((a) => a.id == albid);
-    if (!l) return r("Album not found.");
-    r({
-      a: res,
-      l,
-    });
+  let albumDetails = new Promise<{ a: Artist; l: Album } | string>(
+    async (r) => {
+      const res = await API.fetchArtist(id);
+      if (res.err) return r(res.message!);
+      const l = res.albums!.find((a) => a.id == albid);
+      if (!l) return r("Album not found.");
+      r({
+        a: res,
+        l,
+      });
 
-    setTimeout(
-      () => window.location.hash.substring(1) && (window.location.hash = window.location.hash),
-      10
-    );
-  });
+      setTimeout(
+        () =>
+          window.location.hash.substring(1) &&
+          (window.location.hash = window.location.hash),
+        10
+      );
+    }
+  );
 
   //TODO: redo this to not use await
   const i = API.onQueueChange(async (e) => {
@@ -43,7 +51,9 @@
         })
       );
       setTimeout(
-        () => window.location.hash.substring(1) && (window.location.hash = window.location.hash),
+        () =>
+          window.location.hash.substring(1) &&
+          (window.location.hash = window.location.hash),
         10
       );
     }
@@ -54,7 +64,7 @@
 {#await albumDetails}
   <Loader />
 {:then res}
-  {#if typeof res == "string"}
+  {#if typeof res == 'string'}
     <div class="text-sm">{res}</div>
   {:else}
     <div class="card w-3/4 bg-base-300 shadow-xl box-border m-auto">
@@ -66,35 +76,31 @@
             </div>
             <span
               class="flex items-center justify-center w-full h-full bg-black
-                bg-opacity-50 absolute rounded"
-              ><span
+                bg-opacity-50 absolute rounded"><span
                 class={`radial-progress ${(() => {
                   const len = res.l.tracks.filter((t) => t.added).length;
-                  if (len == res.l.tracks.length) return "text-success";
-                  else if (len == 0) return "text-error";
-                  else return "text-warning";
+                  if (len == res.l.tracks.length) return 'text-success';
+                  else if (len == 0) return 'text-error';
+                  else return 'text-warning';
                 })()}`}
-                style={`--value:${
-                  (res.l.tracks.filter((t) => t.added).length / res.l.tracks.length) * 100
-                };--size:7rem;--thickness:0.5rem;`}
-              >
+                style={`--value:${(res.l.tracks.filter((t) => t.added).length / res.l.tracks.length) * 100};--size:7rem;--thickness:0.5rem;`}>
                 {res.l.tracks.filter((t) => t.added).length}/{res.l.tracks.length}
-              </span></span
-            >
+              </span></span>
           </div>
           <div class="flex flex-col gap-1 ml-1">
             <div class="flex items-center gap-1">
               <div class="text-3xl">{res.l.name} ({res.l.year})</div>
-              <a class="text-secondary" href={res.l.url} target="_blank" rel="noreferrer"
-                ><IconExternalLink size={32} /></a
-              >
+              <a
+                class="text-secondary"
+                href={res.l.url}
+                target="_blank"
+                rel="noreferrer"><IconExternalLink size={32} /></a>
             </div>
             <div class="flex items-center gap-1">
               <a
                 class="flex items-center gap-1 text-sm"
                 href={`/artists/${res.a.id}/manage`}
-                use:link
-              >
+                use:link>
                 {#if res.a.icon}
                   <div class="avatar">
                     <div class="w-5 rounded-full">
@@ -105,14 +111,13 @@
                   <div class="avatar placeholder">
                     <div
                       class="w-5 rounded-full bg-neutral-focus
-                        text-neutral-content"
-                    >
+                        text-neutral-content">
                       <span class="text-xs">
                         {res.a.name
                           .split(/ +/g)
                           .slice(0, 1)
                           .map((a) => a[0].toUpperCase())
-                          .join("")}
+                          .join('')}
                       </span>
                     </div>
                   </div>
@@ -154,8 +159,8 @@
                   seconds: Math.floor(track.duration / 1000),
                 })
                   .normalize()
-                  .toFormat("mm:ss")
-                  .replace(/,/g, "")}
+                  .toFormat('mm:ss')
+                  .replace(/,/g, '')}
               </td>
               <td class="text-right">
                 {#if track.added}
@@ -163,27 +168,26 @@
                     class="btn btn-square btn-sm btn-error"
                     on:click={async (e) => {
                       //@ts-ignore
-                      e.target.classList.add("btn-outline");
-                      await API.post("/track_delete", {
+                      e.target.classList.add('btn-outline');
+                      await API.post('/track_delete', {
                         artist: res.a.id,
                         album: res.l.id,
                         track: track.id,
                       });
-                    }}
-                  >
+                    }}>
                     <IconTrash />
                   </div>
-                {:else}
-                  <a class="btn btn-square btn-sm btn-primary" href={`#${track.id}`}>
-                    <IconDownload />
-                  </a>
                 {/if}
+                <a
+                  class="btn btn-square btn-sm {track.added ? 'btn-success' : 'btn-primary'}"
+                  href={`#${track.id}`}>
+                  <IconDownload />
+                </a>
                 <a
                   class="btn btn-square btn-sm btn-secondary"
                   href={track.url}
                   target="_blank"
-                  rel="noreferrer"
-                >
+                  rel="noreferrer">
                   <IconExternalLink />
                 </a>
               </td>
