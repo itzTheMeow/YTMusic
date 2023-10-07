ab0b58af7430339d59ca112f1dd7241eda9d287f
Meow <me@itsmeow.cat>
2023-10-07 13:46:47 -0400

    move download

--- server/sound/main.go
+++ server/sound/main.go
@@ -1,6 +1,10 @@
 package sound
 
-import "github.com/itzTheMeow/YTMusic/types"
+import (
+	"errors"
+
+	"github.com/itzTheMeow/YTMusic/types"
+)
 
 func Search(provider types.SoundProvider, query string) []types.Downloadable {
 	switch provider {
@@ -10,5 +14,16 @@ func Search(provider types.SoundProvider, query string) []types.Downloadable {
 		return SearchSoundCloud(query)
 	default:
 		return nil
+	}
+}
+
+func Download(provider types.SoundProvider, url string) (*string, error) {
+	switch provider {
+	case types.SoundProviderYouTube:
+		return DownloadYouTube(url)
+	case types.SoundProviderSoundCloud:
+		return DownloadSoundcloud(url)
+	default:
+		return nil, errors.New("Invalid provider ID.")
 	}
 }
